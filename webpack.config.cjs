8f1d2ba616195639cdcc8ccbb1fabdab43faf3f4
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2024-07-16 23:21:32 -0300

    chore: add webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,28 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.ts',
+  module: {
+    rules: [
+      { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" }
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"]
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+  ],
+};
